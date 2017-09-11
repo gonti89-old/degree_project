f412b9b96cc89ae6f7a8a36ae160f8b89c16ff97
Kamil Gontarz <kamil.gontarz@tlen.pl>
2017-09-11 23:28:36 +0200

    Remove ignored files

--- tools/init_db.sh
+++ tools/init_db.sh
@@ -1,2 +1,49 @@
+#!/bin/bash
+cd $(dirname $0)
+mongod=../venv/bin/mongod
+mongod_data=../data/db
+mongod_log=../data/db/log/mongodb/mongod.log
+prog=init_db.sh
+RETVAL=0
 
-mongod --dbpath data/db
+stop() {
+    grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
+    if [ ${#grep_mongo} -gt 0 ]
+    then
+	echo "Stop MongoDB."
+	PID=`ps x | grep -v grep | grep "${mongod}" | awk '{ print $1 }'`
+	`kill -2 ${PID}`
+	RETVAL=$?
+    else
+	echo "MongoDB is not running."
+    fi
+}
+start() {
+    grep_mongo=`ps aux | grep -v grep | grep "${mongod}"`
+    if [ -n "${grep_mongo}" ]
+    then
+	echo "MongoDB is already running."
+    else
+	echo "Start MongoDB."
+	`${mongod} --dbpath ${mongod_data} --logpath ${mongod_log} --fork --logappend`
+	RETVAL=$?
+    fi
+}
+
+case "$1" in
+    start)
+	start
+	;;
+    stop)
+	stop
+	;;
+    restart)
+	stop
+	start
+	;;
+    *)
+	echo $"Usage: $prog {start|stop|restart}"
+	exit 1
+esac
+
+exit $RETVAL
