9b1555dccbe722a01e2302a10f8ffe67e0855151
Kamil Gontarz <kamil.gontarz@tlen.pl>
2017-09-16 02:45:31 +0200

    adding date tools

--- interface/app/tools.py
+++ interface/app/tools.py
@@ -1,3 +1,5 @@
+import datetime
+
 def get_instances():
     instances_list = [(x, x) for x in ['prod', 'beta']]
     return instances_list
@@ -8,7 +10,7 @@ def get_period_types():
 
 def get_countries():
     countries = [(country_name, country_name) for country_name in
-                 ['Polska', 'Croatia', 'Ukraine', 'Turkey']]
+                 ['Poland', 'Croatia', 'Ukraine', 'Turkey']]
     return countries
 
 def get_reports_statuses():
@@ -20,3 +22,14 @@ def get_reports_statuses():
 def get_platforms():
     platforms = [(x,x) for x in [1, 3, 5]]
     return platforms
+
+def parse_date(date):
+    date = date.split('-')
+    year = int(date[0])
+    month = int(date[1])
+    day = int(date[2])
+    return year,month,day
+
+def cast_to_date(date_str):
+    y,m,d = parse_date(date_str)
+    return datetime.date(y,m,d)
