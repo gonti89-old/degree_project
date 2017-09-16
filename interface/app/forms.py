899ab406fbc71e6a223f9f5dc8f482a2c1f463cc
Kamil Gontarz <kamil.gontarz@tlen.pl>
2017-09-16 02:34:53 +0200

    moving choices form definition to From class

--- interface/app/forms.py
+++ interface/app/forms.py
@@ -1,22 +1,26 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, RadioField, SelectField
+from wtforms import StringField, BooleanField, RadioField, SelectField, Form
 from wtforms.fields.html5 import DateField
 from wtforms.validators import DataRequired
 import datetime
+import tools
 
 
 class navigationForm(FlaskForm):
     instances = SelectField('instance',
-                           validators=[DataRequired()],
-                           default='instance_prod')
+                            validators=[DataRequired()],
+                            default='prod',
+                            choices=tools.get_instances())
     dt = DateField('Date',
                    format='%Y-%m-%d',
                    default=datetime.date.today() - datetime.timedelta(days=1),
                    validators=[DataRequired()])
 
     periodType = SelectField('period',
-                            validators=[DataRequired()],
-                            default='period_day')
+                             validators=[DataRequired()],
+                             default='period_day',
+                             choices=tools.get_period_types())
 
     country = SelectField('country',
-                             validators=[DataRequired()])
+                          validators=[DataRequired()],
+                          choices=tools.get_countries())
