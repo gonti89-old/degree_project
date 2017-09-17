2b7fab98948a18531ca3cd9864d424c254f8e3c9
Kamil Gontarz <kamil.gontarz@tlen.pl>
2017-09-17 02:23:14 +0200

    code refactoring, preparing to one report function

--- interface/app/views.py
+++ interface/app/views.py
@@ -22,7 +22,7 @@ def general_report_site(form, statuses, plan_id):
                            plan_id=plan_id)
 
 def read_session_form():
-    dt=tools.cast_to_date(session['data']['dt'])
+    dt = tools.cast_to_date(session['data']['dt'])
     form = forms.navigationForm(instances=session['data']['instances'],
                                 country=session['data']['country'],
                                 periodType=session['data']['periodType'],
@@ -53,19 +53,66 @@ def instance_type():
                            title='inital site',
                            form=form)
 
-@app.route('/error', methods=['POST', 'GET'])
-def error_site():
+#@app.route('/error', methods=['POST', 'GET'])
+#def error_site():
+#    form = read_session_form()
+#    reports_status = tools.get_reports_statuses()
+#    plan_id = 1
+#    if form.validate_on_submit():
+#        return general_report_site(form, reports_status, plan_id)
+
+#    return render_template('error_site.html',
+#                           title='error',
+#                           form=form,
+#                           statuses=reports_status,
+#                           plan_id=plan_id
+#                           )
+
+@app.route('/reports/<report_name>', methods=['POST', 'GET'])
+def reports(report_name):
+
     form = read_session_form()
+
     reports_status = tools.get_reports_statuses()
     plan_id = 1
     if form.validate_on_submit():
-        return general_report_site(form, reports_status, plan_id)
+       return general_report_site(form, reports_status, plan_id)
 
-    return render_template('error_site.html',
-                           title='error',
+    country = request.args.get('country')
+    current_date = request.args.get('dt')
+    period_type = request.args.get('periodType')
+    instance_type = request.args.get('instances')
+    plan_id = int(request.args.get('plan_id'))
+
+    report_api = apiReports(country=country,
+                            report_name=report_name,
+                            date=current_date,
+                            period_type=period_type,
+                            instance_type=instance_type,
+                            plan_id=plan_id)
+    try:
+        data = report_api.create_report()
+
+    except ValueError as e:
+
+        return render_template('error_site.html',
+                               title='error',
+                               form=form,
+                               statuses=reports_status,
+                               url_params=request.args,
+                               error_message=e,
+                               plan_id=plan_id
+                               )
+    platforms = report_api.find_plans_id('on')
+
+    return render_template('first_report.html',
+                           name=report_name,
                            form=form,
+                           plan_id=plan_id,
                            statuses=reports_status,
-                           plan_id=plan_id
+                           platforms=platforms,
+                           url_params=request.args,
+                           data=data
                            )
 
 
@@ -84,6 +131,7 @@ def second_report():
     period_type = request.args.get('periodType')
     instance_type = request.args.get('instances')
     plan_id = int(request.args.get('plan_id'))
+
     report_api = apiReports(country=country,
                             report_name="basicStatsTrend",
                             date=current_date,
@@ -105,13 +153,6 @@ def second_report():
                                )
     platforms = report_api.find_plans_id('on')
 
-    chartID = data['title']['text']
-    series = data['series']
-    chart_title = data['title']
-    chart = data['chart']
-    xAxis = data['xAxis']
-    yAxis = {'title': {'text': 'testlabel'}}
-
     return render_template('first_report.html',
                            name='second_report',
                            form=form,
@@ -119,30 +160,29 @@ def second_report():
                            statuses=reports_status,
                            platforms=platforms,
                            url_params=request.args,
-                           chartID=chartID,
-                           series=series,
-                           chart_title=chart_title,
-                           chart=chart,
-                           xAxis=xAxis,
-                           yAxis=yAxis
+                           data=data
                            )
 
 
-@app.route('/first_report', methods=['POST', 'GET'])
-def first_report():
+@app.route('/test_report', methods=['POST', 'GET'])
+def test_report():
     form = read_session_form()
     reports_status = tools.get_reports_statuses()
     platforms = tools.get_platforms()
 
-    chartID = 'chart_ID'
     chart_type = 'bar'
     chart_height = 350
-    #
     chart = {"type": chart_type, "height": chart_height}
     series = [{"name": 'Label1', "data": [1, 2, 3]}, {"name": 'Label2', "data": [4, 5, 6]}]
-    chart_title = {"text": 'My Title'}
+    chart_title = {"text": 'My_Title'}
     xAxis = {"categories": ['xAxis Data1', 'xAxis Data2', 'xAxis Data3']}
     yAxis = {"title": {"text": 'yAxis Label'}}
+
+    data = {'xAxis': xAxis,
+            'yAxis': yAxis,
+            'title': chart_title,
+            'series': series,
+            'chart': chart}
 
     if form.validate_on_submit():
         return redirect(url_for('reports',
@@ -158,10 +198,4 @@ def first_report():
                            statuses=reports_status,
                            platforms=platforms,
                            url_params=request.args,
-                           chartID=chartID,
-                           series=series,
-                           chart_title=chart_title,
-                           chart=chart,
-                           xAxis=xAxis,
-                           yAxis=yAxis
-                           )
+                           data=data)
