705d6b7d3a8e37fca98123cfb5eb71b4a0956f56
Kamil Gontarz <kamil.gontarz@tlen.pl>
2017-09-18 00:56:42 +0200

    configuration to db_writer- for fake data

--- db_writer/scripts/settings.py
+++ db_writer/scripts/settings.py
@@ -1,8 +1,9 @@
-on_api =['printTree', 'universes.raw', 'universes.www']
-gem_api = ['GetStats']
+on_api =['file1', 'file2']
+gem_api = []
+instance = "prod"
 all_files = on_api + gem_api
 settings = dict(onApi=on_api,
                 gemApi=gem_api,
                 files_to_db_update=all_files,
-                instance_type="prod")
+                instance_type=instance)
 
